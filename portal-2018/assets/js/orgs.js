ed5c750939c8fb52baf468ee3916a886eac1987c
Andreas Martin <andreas.martin@andreasmartin.ch>
2018-02-20 16:56:57 +0100

    Update

--- portal-2018/assets/js/orgs.js
+++ portal-2018/assets/js/orgs.js
@@ -1,11 +1,23 @@
 var orgs = [
-  {
+  /*{
       "name": "DigiBP",
       "type": "org",
       "link": "https://github.com/DigiBP"
-  }/*,
+  },*/
   {
       "name": "DigiBP/digibp-pizza",
       "type": "repo"
-  }*/
+  },
+  {
+    "name": "DigiBP/digibp-camunda-examples",
+    "type": "repo"
+  },
+  {
+    "name": "DigiBP/digibp-social-media-management",
+    "type": "repo"
+  },
+  {
+    "name": "DigiBP/digibp-mscbisadmission-camunda-showcase",
+    "type": "repo"
+  }
 ];
